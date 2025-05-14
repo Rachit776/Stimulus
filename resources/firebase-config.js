d0b04b2c5af166cdec1c6693ce73b9d3c00aa769
Rachit776 <rachitkumar776@gmail.com>
2025-05-14 18:30:42 +0530

    Error Check

--- resources/firebase-config.js
+++ resources/firebase-config.js
@@ -1,8 +1,8 @@
 
 var firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  storageBucket: ""
+  apiKey: "AIzaSyDWLnXQwRR-kmvSwlWxgt_TX2F_uEDjHlk",
+  authDomain: "minor-1709364961391.firebaseapp.com",
+  databaseURL: "https://minor-1709364961391-default-rtdb.firebaseio.com",
+  storageBucket: "minor-1709364961391.appspot.com",
 };
 firebase.initializeApp(firebaseConfig);
